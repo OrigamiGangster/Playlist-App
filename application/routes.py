fb7a6666149d98511985592ba0017b71e28d3e5b
Peter Hessler <peterhessler@hotmail.co.uk>
2022-03-10 05:57:04 +0000

    flask button on home page

--- application/routes.py
+++ application/routes.py
@@ -2,9 +2,14 @@ from flask import render_template, request
 from application import app
 from application.forms import AlbumForm
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    return render_template('home.html')
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    form = AlbumForm(request.form)
+    if request.method == 'POST':
+        if request.form.get('action1') == 'AddAlbum':
+            return render_template('new_album.html', form=form)
+    
+    return render_template("home.html")
 
 @app.route('/new_album', methods=['GET', 'POST'])
 def new_album():
